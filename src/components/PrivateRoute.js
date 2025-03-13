128bbf5e9ac13d816868f30a73173eaffefdb1bd
MuriloApa <murilo.aparecido@ufms.br>
2025-03-13 12:28:32 -0400

    Ajustes nas rotas

--- src/components/PrivateRoute.js
+++ src/components/PrivateRoute.js
@@ -3,5 +3,5 @@ import { Navigate, Outlet } from "react-router-dom";
 export default function PrivateRoute() {
   const token = localStorage.getItem("token");
 
-  return token ? <Outlet /> : <Navigate to="/" />;
+  return token ? <Outlet /> : <Navigate to="/login" />;
 }
--- src/pages/page.js
+++ src/pages/page.js
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import Navbar from '../components/layout/navbar/navbar';
 import { Outlet } from 'react-router-dom';
-import Home from '../components/Home';
+import Home from './Home';
 
 export default function Page() {
     return <>
